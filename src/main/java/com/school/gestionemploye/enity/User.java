959e01298e60b4f14ad9c2b137f7d6742602768a
mohamed <m.eddinari5197@uca.ac.ma>
2026-08-02 23:29:48 +0200

    feat :add userRepository

--- src/main/java/com/school/gestionemploye/enity/User.java
+++ src/main/java/com/school/gestionemploye/enity/User.java
@@ -1,9 +1,9 @@
 package com.school.gestionemploye.enity;
 
-import com.fasterxml.jackson.annotation.JsonTypeId;
 
-@Entity
-@Table
+
+
+
 public class User {
 
 }
---
+++ src/main/java/com/school/gestionemploye/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.school.gestionemploye.repository;
+
+public interface UserRepository {
+}
