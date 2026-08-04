ef5bc95bec3de79a1131592f6124252a0ecda00c
mohamed <m.eddinari5197@uca.ac.ma>
2026-08-04 23:34:36 +0200

    add generateToken method

--- src/main/java/com/school/gestionemploye/security/JwtService.java
+++ src/main/java/com/school/gestionemploye/security/JwtService.java
@@ -2,12 +2,15 @@ package com.school.gestionemploye.security;
 
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Base64;
+import java.util.Date;
 
 @Service
 public class JwtService {
@@ -25,5 +28,14 @@ public class JwtService {
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
 
 }
